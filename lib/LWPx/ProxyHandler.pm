06515c8b06444be82b9101458807ca4780230b31
Toru Yamaguchi <zigorou@cpan.org>
2013-02-21 01:44:35 +0900

    initial import

---
+++ lib/LWPx/ProxyHandler.pm
@@ -0,0 +1,41 @@
+package LWPx::ProxyHandler;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+LWPx::ProxyHandler - ...
+
+=head1 SYNOPSIS
+
+  use LWPx::ProxyHandler;
+
+=head1 DESCRIPTION
+
+LWPx::ProxyHandler is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Toru Yamaguchi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+
+use_ok $_ for qw(
+    LWPx::ProxyHandler
+);
+
+done_testing;
